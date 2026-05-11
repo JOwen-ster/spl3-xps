bc42e20a3222527b7a4a92b4b0ec0795e9b2ab18
JOwen-ster <jowen.sterling@csu.fullerton.edu>
2026-05-11 00:06:49 -0700

    Update CICD

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   output: "export",
   distDir: "dist",
-  basePath: '/spl3-xps',
+  basePath: isProd ? '/spl3-xps' : '',
   images: {
     unoptimized: true,
   },
